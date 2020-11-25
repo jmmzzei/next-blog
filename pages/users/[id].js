eaed48ba23d8c6266cd94f9fe57ccb77623b6a2e
José María <josemmazzei@gmail.com>
2020-11-25 17:57:04 -0300

    Add pre-rendered paths in dynamic routes

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,5 +1,38 @@
-import { useRouter } from 'next/router'
-export default () => {
-  const router = useRouter()
-  return <div>UserID is: {router.query.id}</div>
+import {useRouter} from 'next/router'
+import Main from '../../components/Main'
+import Detail from '../../components/Detail'
+
+export default ({user}) => {
+  
+  const router = useRouter({user})
+  return (
+    <Main>
+      {router.isFallback 
+        ?<Detail />
+        :<Detail title={user.name} body={user.email} />
+      } 
+    </Main>
+  )
+}
+
+export async function getStaticProps({params}) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+  if (res.ok) {
+    const user = await res.json()
+    return { props: { user } }
+  }
+  return {}
+}
+
+export async function getStaticPaths(){
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/`)
+  if (res.ok) {
+    const users = await res.json()
+    const paths = users.map(e => ({params: {id: e.id.toString() }}))   
+    return {
+      paths , 
+      fallback: true
+    } 
+  }
+  return {}
 }
