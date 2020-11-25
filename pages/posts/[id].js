5622001b5e70e4f777f094f19d11a22ab44f7df2
José María <josemmazzei@gmail.com>
2020-11-25 15:45:52 -0300

    Apply SSR on single post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,21 @@
-import { useRouter } from 'next/router'
+import Main from '../../components/Main'
+import Detail from '../../components/Detail'
 
-export default () => {
-  const router = useRouter()
-  return <div>Posts ID is: {router.query.id}</div>
+export default ({ post }) => {
+  return (
+    <Main>
+      <Detail title={post.title} body={post.body} />
+    </Main>
+  )
+}
+
+export async function getServerSideProps({ params }) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`,
+  )
+  if (res.ok) {
+    const post = await res.json()
+    return { props: { post: post } }
+  }
+  return {}
 }
