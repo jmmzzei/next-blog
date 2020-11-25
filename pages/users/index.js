7f6f09b2a001395694292f509eac214390326ab2
José María <josemmazzei@gmail.com>
2020-11-25 16:14:31 -0300

    Change rendering strategy to SG in users page

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,1 +1,24 @@
-export default () => <div>Users</div>
+import User from '../../components/User'
+import Grid from '../../components/Grid'
+import Main from '../../components/Main'
+
+export default ({ users }) =>(
+  <Main>
+  <Grid>
+    {
+  users.map(e => (
+    <User key={e.id} name={e.name} id={e.id} phone={e.phone} email={e.email} />
+  ))
+    }
+  </Grid>
+  </Main>
+  )
+
+export async function getStaticProps() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/`)
+  if (res.ok) {
+    const users = await res.json()
+    return { props: { users } }
+  }
+  return {}
+}
